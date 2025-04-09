5546c406ce5e1030c38561e520e938943bc03f41
xwRongx <57518285+xwRongx@users.noreply.github.com>
2025-04-09 15:29:46 -0700

    added footer image

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,5 +1,15 @@
+import cloudfooter from "@/public/logos/cloudfooter.svg";
+//import logo from "@/public/logos/logo.svg";
+//import Link from "next/link";
+import Image from "next/image";
+//import {links} from "../data/footerLinks";
+
 const Footer = () => {
-  return <div className="flex items-center">Footer</div>;
+  return (
+    <div className="flex w-full items-center">
+      <Image src={cloudfooter} alt="cloudfooter" className="w-screen" />
+    </div>
+  );
 };
 
 export default Footer;
