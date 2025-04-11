d935abd2f1a0af81bbc51b09b999f22853723873
Richard Zhou <rzhou052@ucr.edu>
2025-04-10 22:39:35 -0700

    Switched to default tailwind font-sizes

--- src/components/board/BoardMemberCard.tsx
+++ src/components/board/BoardMemberCard.tsx
@@ -21,16 +21,14 @@ const BoardMemberCard: React.FC<BoardMemberCardProps> = ({
   return (
     <div className="justify-items-center">
       <Image src={photo} alt={`Photo of ${name}`} />
-      <div className="text-wushu-red-100 text-[45px] leading-24 font-bold italic">
+      <div className="text-wushu-red-100 text-5xl leading-24 font-bold italic">
         {/* still need to add arimo font? not sure if it hasnt been added yet or smth */}
         {title}
       </div>
-      <div className="text-wushu-red-100 text-[40px] leading-0 italic">
-        {name}
-      </div>
+      <div className="text-wushu-red-100 text-4xl leading-0 italic">{name}</div>
       <br />
       <br />
-      <div className="text-wushu-gold-200 text-center text-[32px] leading-10">
+      <div className="text-wushu-gold-200 text-center text-3xl leading-10">
         {year} <br />
         {major} <br />
         {pronouns} <br />
