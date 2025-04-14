81f3fdfa637000a44b83e95a54821003e65ff0d0
xwRongx <57518285+xwRongx@users.noreply.github.com>
2025-04-14 03:35:50 -0700

    attempt map array to icons

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,13 +1,21 @@
 import cloudfooter from "@/public/logos/cloudfooter.svg";
 //import logo from "@/public/logos/logo.svg";
-//import Link from "next/link";
+import Link from "next/link";
 import Image from "next/image";
-//import {links} from "../data/footerLinks";
+import { links } from "@/data/footer";
 
 const Footer = () => {
   return (
     <div className="flex w-full items-center">
       <Image src={cloudfooter} alt="cloudfooter" className="w-screen" />
+      <div className="flex justify-end">
+        {links.map((item, index) => (
+          <Link href={item.link} target="_blank" key={index} className="">
+            {item.icon}
+          </Link>
+          //<div></div>
+        ))}
+      </div>
     </div>
   );
 };
---
+++ src/data/footer.ts
@@ -0,0 +1,24 @@
+import icon from "@/public/logos/gmail.svg";
+
+export const links = [
+  {
+    text: "email",
+    icon: icon,
+    link: "google.com",
+  },
+  {
+    text: "youtube",
+    icon: icon,
+    link: "google.com",
+  },
+  {
+    text: "discord",
+    icon: icon,
+    link: "google.com",
+  },
+  {
+    text: "instagram",
+    icon: icon,
+    link: "google.com",
+  },
+];
