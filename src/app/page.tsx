42381ffdfcddd27cbac776dc0c7be35984f1f9d6
javi <javi@ucr-secure-43-10-13-223-66.wnet.ucr.edu>
2025-04-15 19:22:50 -0700

    Updated fonts

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,8 +2,7 @@ import Example from "@/components/Example";
 
 const Home = () => {
   return (
-    <div className="flex h-screen w-screen items-center justify-center">
-      Hello world
+    <div className="font-arimo flex h-screen w-screen items-center justify-center">
       <Example text1="Adjust" text2="these" text3="Props" />
     </div>
   );
