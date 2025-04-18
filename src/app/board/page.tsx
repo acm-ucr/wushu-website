bbb71fc3488b95ffd6dc1b79fd827b598ac4d37c
Richard Zhou <rzhou052@ucr.edu>
2025-04-17 22:22:03 -0700

    Mapping through, need to work on formatting and spacing. Also added data folder for storing board member data

--- src/app/board/page.tsx
+++ src/app/board/page.tsx
@@ -1,7 +1,22 @@
+import BoardMemberCard from "../../components/board/BoardMemberCard";
+import { BoardMembers } from "../../data/BoardMembers";
+
 const Board = () => {
   return (
-    <div className="flex h-screen w-screen items-center justify-center">
-      Board
+    <div className="grid grid-cols-3 justify-items-center">
+      {BoardMembers.map((Member) => (
+        <div className="max-w-[348px]" key={Member.key}>
+          <BoardMemberCard
+            key={Member.key}
+            photo={Member.photo}
+            position={Member.position}
+            name={Member.name}
+            year={Member.year}
+            major={Member.major}
+            pronouns={Member.pronouns}
+          />
+        </div>
+      ))}
     </div>
   );
 };
--- src/components/board/BoardMemberCard.tsx
+++ src/components/board/BoardMemberCard.tsx
@@ -3,7 +3,7 @@ import Image, { StaticImageData } from "next/image";
 
 interface BoardMemberCardProps {
   photo: StaticImageData;
-  title: string;
+  position: string;
   name: string;
   year: string;
   major: string;
@@ -12,18 +12,17 @@ interface BoardMemberCardProps {
 
 const BoardMemberCard: React.FC<BoardMemberCardProps> = ({
   photo,
-  title,
+  position,
   name,
   year,
   major,
   pronouns,
 }) => {
   return (
-    <div className="justify-items-center">
+    <div className="justify-items-center font-arimo -tracking-[0.1em] whitespace-nowrap">
       <Image src={photo} alt={`Photo of ${name}`} />
       <div className="text-wushu-red-100 text-5xl leading-24 font-bold italic">
-        {/* still need to add arimo font? not sure if it hasnt been added yet or smth */}
-        {title}
+        {position}
       </div>
       <div className="text-wushu-red-100 text-4xl leading-0 italic">{name}</div>
       <div className="text-wushu-gold-200 flex flex-col pt-12 text-center text-3xl leading-10">
