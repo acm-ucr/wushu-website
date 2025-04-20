9594bb43e25c0512fde8cebc8b89d3d91c0d2479
feliciaongn <feliciaongn@gmail.com>
2025-04-19 18:07:49 -0700

    Fixed fonts Location and Times

--- src/components/home/LocationAndTime.tsx
+++ src/components/home/LocationAndTime.tsx
@@ -3,24 +3,24 @@ import Image from "next/image";
 
 const LocationAndTime = () => {
   return (
-    <div className="bg-wushu-cream-100 flex w-2/3 gap-1 rounded-md p-10">
+    <div className="bg-wushu-cream-100 flex w-2/3 rounded-md p-10">
       <div className="flex w-2/3 flex-col justify-items-center gap-1 rounded-md p-10">
         <div>
-          <h1 className="text-wushu-gold-100 top-left text-5xl font-extrabold">
-            Location and Times
+          <h1 className="text-wushu-gold-100 top-left font-margasa text-5xl font-extrabold">
+            LOCATION AND TIMES
           </h1>
           <Image
             src="/images/who_we_are/location_and_times_img.webp"
             alt="Location and Times (shows club participants all participating in an activity)"
-            width={1000}
-            height={300}
+            width={3000}
+            height={3000}
           />
         </div>
       </div>
-      <div className="text-wushu-red-100 flex w-2/3 flex-col justify-items-start gap-1 p-10 pt-10 pb-3 text-3xl font-bold">
-        <h2>Location:</h2>
+      <div className="text-wushu-red-100 font-arimo flex w-2/5 flex-col pt-10 text-[24px] tracking-tighter">
+        <h2 className="pt-20">Location:</h2>
         <h2>MPR C</h2>
-        <h2 className="pt-10">Times:</h2>
+        <h2 className="pt-5">Times:</h2>
         <h2>Wednesday 3:00-5:00PM</h2>
         <h2>Sunday 5:00-7:00PM</h2>
       </div>
