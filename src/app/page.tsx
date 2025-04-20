976dd38a7c0a035191d6db1044ca0d8a1676df7a
Chris Jung <141481670+chrisjung0919@users.noreply.github.com>
2025-04-19 19:21:02 -0700

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,5 @@
-import Example from "@/components/contact/ContactUs";
+import MeetTheBoard from "@/components/board/BoardMemberCard";
 
-const Home = () => {
-  return (
-    <div className="flex h-screen w-screen items-center justify-center">
-      <Example text1="Adjust" text2="these" text3="Props" />
-    </div>
-  );
-};
-
-export default Home;
+export default function Home() {
+  return <MeetTheBoard />;
+}
