5c0239ab80157616a4e30e2fc317c80360b4304b
Chris Jung <141481670+chrisjung0919@users.noreply.github.com>
2025-04-21 13:13:22 -0700

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,5 @@
-import Example from "@/components/Example";
+import MeetTheBoard from "@/components/board/BoardMemberCard";
 
-const Home = () => {
-  return (
-    <div className="font-arimo flex h-screen w-screen items-center justify-center">
-      <Example text1="Adjust" text2="these" text3="Props" />
-    </div>
-  );
-};
-
-export default Home;
+export default function Home() {
+  return <MeetTheBoard />;
+}
