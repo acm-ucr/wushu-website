fedb7a78c12d9b45e185562999cc28e15a2fa5f4
Simon Cun <simoncun2586@gmail.com>
2025-04-21 17:21:06 -0700

    changed button to example email with default text

--- src/components/contact/WantToBookUs.tsx
+++ src/components/contact/WantToBookUs.tsx
@@ -3,6 +3,16 @@ import Link from "next/link";
 import Image from "next/image";
 import GroupPhoto from "@/public/images/contact_us/want_to_book_us.webp";
 import WushuLogo from "@/public/logos/wushufront.svg";
+
+const email = `mailto:example@email.com?subject=${encodeURIComponent("Wushu Booking Request")}&body=${encodeURIComponent(`Hi Wushu Team,
+
+  I would like to book you for an event. Here are the details:
+  - Time:
+  - Location:
+  - Other info:
+  
+  Thanks!`)}`;
+
 const WantToBookUs = () => {
   return (
     <div>
@@ -10,13 +20,13 @@ const WantToBookUs = () => {
       <div className="flex">
         <div>
           <div className="font-arimo text-wushu-red-100 mr-8 w-[45vw] pt-8 text-4xl">
-            Please fill out the form and provide us with a time, location, and
-            any other details!
+            Please email us at example@email.com and provide us with a time,
+            location, and any other details!
           </div>
           <div className="flex h-[80%] w-[80%] items-center justify-center">
             <Link
               className="border-wushu-gold-100 flex h-[24vh] w-[24vw] items-center justify-center rounded-2xl border-6 bg-white"
-              href="book-us"
+              href={email}
             >
               <Image
                 src={WushuLogo}
@@ -24,7 +34,7 @@ const WantToBookUs = () => {
                 className="h-[20vh] object-contain"
               />
               <div className="text-wushu-red-100 font-amaranth flex w-[110%] text-left text-4xl font-bold">
-                BOOK US HERE!
+                Email Us!
               </div>
             </Link>
           </div>
