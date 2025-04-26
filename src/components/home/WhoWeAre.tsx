ba2153490dcceb23b0dae431c37fc17d0b3de113
feliciaongn <feliciaongn@gmail.com>
2025-04-26 15:31:07 -0700

    Trying to fix staged files for Who We Are

--- src/components/home/WhoWeAre.tsx
+++ src/components/home/WhoWeAre.tsx
@@ -1,7 +1,38 @@
 import React from "react";
+import Image from "next/image";
 
 const WhoWeAre = () => {
-  return <div>Who We Are</div>;
+  return (
+    <div className="bg-wushu-cream-100 flex w-2/3 content-start gap-1 p-10">
+      <div className="flex w-2/3 flex-col gap-1 p-10">
+        <div>
+          <h1 className="text-wushu-gold-200 font-margasa text-5xl font-extrabold">
+            WHO WE ARE
+          </h1>
+          <Image
+            src="/images/who_we_are/who_we_are_img.webp"
+            alt="Who We Are"
+            width={585}
+            height={575}
+          />
+        </div>
+      </div>
+      <div className="flex w-1/2 flex-col">
+        <h2 className="text-wushu-gold-100 font-arimo pt-25 pb-1 text-3xl font-bold">
+          WHAT IS WUSHU?
+        </h2>
+        <h3 className="text-wushu-red-100 font-arimo text-[24px] font-[500] tracking-tighter">
+          Wushu, also known as kung fu, is a competitive Chinese martial art. It
+          integrates concepts and forms from various traditional and modern
+          Chinese martial arts, including Shaolin kung fu, tai chi, and
+          Wudangquan. "Wushu" is the Chinese term for "martial arts" (武 "Wu"=
+          combat or martial, 術 "Shu" = art), it reflects the art's goal as a
+          compilation and standardization of various styles. To distinguish it
+          from traditional Chinese martial arts, it's called "Modern Wushu."
+        </h3>
+      </div>
+    </div>
+  );
 };
 
 export default WhoWeAre;
