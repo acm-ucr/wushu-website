6f6a2756abe98f6d825034cdc3ea6fedaecc8f6f
xwRongx <57518285+xwRongx@users.noreply.github.com>
2025-04-28 06:52:43 -0700

    formatted

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,6 +1,8 @@
-const Header = (props:{title:string}) => {
+const Header = (props: { title: string }) => {
   return (
-    <div className="text-wushu-red-300 text-9xl font-margasa justify-start">{props.title}</div>
+    <div className="text-wushu-red-300 font-margasa justify-start text-9xl">
+      {props.title}
+    </div>
   );
 };
 
