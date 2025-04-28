e4afb9487cce27baa697533532d54780aba1e7c6
mdiep010 <mdiep010@ucr.edu>
2025-04-27 21:26:45 -0700

    Added mobile responsiveness to 404 Page

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -6,14 +6,13 @@ const NotFound = () => {
     <div className="bg-wushu-cream-100 flex min-h-screen flex-col items-center justify-center">
       <Image
         src={errorPic}
-        alt="Wushu @ UCR logo with a crane posed above holding a sword."
-        width={400}
-        height={400}
+        alt="Wushu @ UCR logo with a crane posed above holding a sword"
+        className="h-auto w-60 sm:w-60 md:w-80 lg:w-100"
       />
-      <div className="text-wushu-red-100 font-arimo p-2 text-center text-5xl font-bold md:text-7xl">
+      <div className="text-wushu-red-100 font-arimo p-2 text-center text-3xl font-bold sm:text-3xl md:text-5xl lg:text-6xl">
         404 Error
       </div>
-      <div className="text-wushu-red-100 font-arimo text-center text-2xl">
+      <div className="text-wushu-red-100 font-arimo sm:text-md text-center text-sm md:text-2xl lg:text-3xl">
         <div>Oops! Sorry we couldn't find the</div>
         <div>page you were looking for!</div>
       </div>
