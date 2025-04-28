9e5e6f949f085e18af89caf6e290d76f17971a99
xwRongx <57518285+xwRongx@users.noreply.github.com>
2025-04-28 06:50:30 -0700

    implemented header component

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,5 +1,7 @@
-const Header = () => {
-  return <div>Header</div>;
+const Header = (props:{title:string}) => {
+  return (
+    <div className="text-wushu-red-300 text-9xl font-margasa justify-start">{props.title}</div>
+  );
 };
 
 export default Header;
