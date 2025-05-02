a3032626998bce1fc16eaa90d3fed58abf34402c
rzhou052 <rzhou052@ucr.edu>
2025-05-02 15:27:50 -0700

    Moved mapping of board members to separate component

--- src/app/board/page.tsx
+++ src/app/board/page.tsx
@@ -1,22 +1,9 @@
-import BoardMemberCard from "../../components/board/BoardMemberCard";
-import { BoardMembers } from "../../data/BoardMembers";
+import BoardMemberMap from "@/components/board/BoardMemberMap";
 
 const Board = () => {
   return (
-    <div className="relative mx-auto grid w-5/6 grid-cols-3 justify-center gap-x-5 gap-y-12 pt-5">
-      {BoardMembers.map((Member) => (
-        <div key={Member.key}>
-          <BoardMemberCard
-            key={Member.key}
-            photo={Member.photo}
-            position={Member.position}
-            name={Member.name}
-            year={Member.year}
-            major={Member.major}
-            pronouns={Member.pronouns}
-          />
-        </div>
-      ))}
+    <div className="">
+      <BoardMemberMap />
     </div>
   );
 };
