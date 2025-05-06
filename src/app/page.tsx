3d1999ad65b9ee222e767b00ac46af5e86e99465
Chris Jung <141481670+chrisjung0919@users.noreply.github.com>
2025-05-05 20:01:07 -0700

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,9 @@
-import WhoWeAre from "@/components/home/WhoWeAre";
+import ContactUs from "@/components/contact/ContactUs";
 
 const Home = () => {
   return (
     <div className="font-arimo flex h-screen w-screen items-center justify-center">
-      <WhoWeAre />
+      <ContactUs />
     </div>
   );
 };
