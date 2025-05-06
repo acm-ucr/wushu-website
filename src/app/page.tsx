bd0b26b138af35be7a259dcf590be569236e3ff2
Chris Jung <141481670+chrisjung0919@users.noreply.github.com>
2025-05-05 18:14:05 -0700

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,11 @@
 import MeetTheBoard from "@/components/board/MeetTheBoard";
 
-export default function Home() {
-  return <MeetTheBoard />;
-}
+const Home = () => {
+  return (
+    <div className="font-arimo flex h-screen w-screen items-center justify-center">
+      <MeetTheBoard />
+    </div>
+  );
+};
+
+export default Home;
