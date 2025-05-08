73d7855259e8245a9cc226cafcc6fc72d3a41ba2
veritasia <aminaapstudent@yahoo.com>
2025-05-07 22:42:56 -0700

    fixing whoweare formatting

--- src/components/home/WhoWeAre.tsx
+++ src/components/home/WhoWeAre.tsx
@@ -3,25 +3,28 @@ import Image from "next/image";
 
 const WhoWeAre = () => {
   return (
-    <div className="bg-wushu-cream-100 flex w-2/3 content-start gap-1 p-10">
-      <div className="flex w-2/3 flex-col gap-1 p-10">
+    <div className="flex w-full content-start gap-1 p-10">
+      <div className="flex w-2/3 flex-col items-center">
         <div>
-          <h1 className="text-wushu-gold-200 font-margasa text-5xl font-extrabold">
+          <div className="text-wushu-gold-200 font-margasa mb-6 text-5xl font-extrabold">
             WHO WE ARE
-          </h1>
-          <Image
-            src="/images/who_we_are/who_we_are_img.webp"
-            alt="Who We Are"
-            width={585}
-            height={575}
-          />
+          </div>
+          <div className="bg-wushu-red-200">
+            <Image
+              src="/images/who_we_are/who_we_are_img.webp"
+              alt="Who We Are"
+              width={585}
+              height={575}
+              className="translate-x-5 -translate-y-5"
+            />
+          </div>
         </div>
       </div>
       <div className="flex w-1/2 flex-col">
-        <h2 className="text-wushu-gold-100 font-arimo pt-25 pb-1 text-3xl font-bold">
+        <h2 className="text-wushu-gold-100 font-arimo pt-25 pb-1 text-4xl font-bold">
           WHAT IS WUSHU?
         </h2>
-        <h3 className="text-wushu-red-100 font-arimo text-2xl tracking-tighter">
+        <h3 className="text-wushu-red-100 font-arimo text-3xl tracking-tighter">
           Wushu, also known as kung fu, is a competitive Chinese martial art. It
           integrates concepts and forms from various traditional and modern
           Chinese martial arts, including Shaolin kung fu, tai chi, and
