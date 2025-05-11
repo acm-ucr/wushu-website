b3f1a723b1b48d99c1b1f11f6d2a905efbcfcaab
Simon <simoncun2586@gmail.com>
2025-05-10 20:02:59 -0700

    initial commit

---
+++ src/components/board/LineBreak.tsx
@@ -0,0 +1,33 @@
+"use client";
+import { motion } from "framer-motion";
+import { Weapons } from "@/data/LineBreak";
+import Image from "next/image";
+
+const LineBreak = () => {
+  const repeatedWeapons = Array(5).fill(Weapons).flat();
+
+  const marqueeAnimation = {
+    animate: { x: "-50%" },
+    transition: {
+      duration: 10,
+      ease: "linear",
+      repeat: Infinity,
+      repeatType: "loop" as const,
+    },
+  };
+
+  return (
+    <div className="relative overflow-hidden py-20 whitespace-nowrap">
+      <motion.div
+        {...marqueeAnimation}
+        className="flex whitespace-nowrap gap-3 w-max"
+      >
+        {[...repeatedWeapons, ...repeatedWeapons].map(({ photo, name }, index) => (
+          <Image key={index} src={photo} alt={name} width={40} height={40} />
+        ))}
+      </motion.div>
+    </div>
+  );
+};
+
+export default LineBreak;
