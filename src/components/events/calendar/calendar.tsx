0153a3ffdf6059e69a1edc069b254039d00bdbe3
CruzL092 <cruzbusiness760@gmail.com>
2025-05-14 13:00:00 -0700

    Dealing with Formatting

--- src/components/events/calendar/calendar.tsx
+++ src/components/events/calendar/calendar.tsx
@@ -3,6 +3,8 @@ import Image from "next/image";
 import React from "react";
 import { Calendar } from "../../ui/calendar";
 
+//
+
 const WushuCalendar = () => {
   return (
     <div className="bg-wushu-cream-100 flex w-full flex-col items-center justify-center lg:pt-64 lg:pb-52">
