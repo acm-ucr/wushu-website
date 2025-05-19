a941a0a72d84f3b3d33c81e3eeb42a5f204a47f5
xwRongx <57518285+xwRongx@users.noreply.github.com>
2025-05-18 20:37:30 -0700

    formattingg

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,14 @@
 import WhoWeAre from "@/components/home/WhoWeAre";
 import Snapshots from "@/components/home/Snapshots";
-import {snapshots} from "@/data/snapshots";
+import { snapshots } from "@/data/snapshots";
 
 const Home = () => {
   return (
     <div className="font-arimo flex h-screen w-screen items-center justify-center">
       <WhoWeAre />
-      <Snapshots photos={snapshots}/>
+      <Snapshots photos={snapshots} />
     </div>
   );
 };
 
 export default Home;
-
