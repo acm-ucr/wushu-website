4e594e787463ad54d6343c573734c0818da50654
xwRongx <57518285+xwRongx@users.noreply.github.com>
2025-05-18 20:35:02 -0700

    formatting

--- src/components/home/Snapshots.tsx
+++ src/components/home/Snapshots.tsx
@@ -5,31 +5,32 @@ import Header from "@/components/Header";
 import leftArrow from "@/public/logos/left-arrow.svg";
 import rightArrow from "@/public/logos/right-arrow.svg";
 
-import { Swiper, SwiperSlide} from 'swiper/react';
+import { Swiper, SwiperSlide } from "swiper/react";
 
-import 'swiper/css';
-import 'swiper/css/effect-coverflow';
-import 'swiper/css/pagination';
-import 'swiper/css/navigation';
+import "swiper/css";
+import "swiper/css/effect-coverflow";
+import "swiper/css/pagination";
+import "swiper/css/navigation";
 
-import { EffectCoverflow, Pagination, Navigation } from 'swiper/modules';
+import { EffectCoverflow, Pagination, Navigation } from "swiper/modules";
 
 interface Photo {
-    src: StaticImageData;
-    alt: string;
+  src: StaticImageData;
+  alt: string;
 }
 
-interface SnapshotsProps { //array of photos
-    photos: Photo[];
+interface SnapshotsProps {
+  //array of photos
+  photos: Photo[];
 }
 
-const Snapshots = ({photos}: SnapshotsProps) => {
-    //const [currIndex, setCurrentIndex] = useState(0);
+const Snapshots = ({ photos }: SnapshotsProps) => {
+  //const [currIndex, setCurrentIndex] = useState(0);
 
-    return(
-        <div className="relative">
-            <Header title={"Snapshots"}></Header>
-            {/* <div className="flex items-center">
+  return (
+    <div className="relative">
+      <Header title={"Snapshots"}></Header>
+      {/* <div className="flex items-center">
                 <div><Image src={leftArrow} alt={"left arrow"} width={100} onClick={scrollNext}></Image></div>
                 <div className="flex">
                     {photos.map((item, index) => (
@@ -38,44 +39,42 @@ const Snapshots = ({photos}: SnapshotsProps) => {
                 </div>
                 <div><Image src={rightArrow} alt={"right arrow"} width={100}></Image></div>  
             </div> */}
-            <div className="flex items-center justify-center gap-7">                
-                <Swiper 
-                    effect={'coverflow'}
-                    
-                    centeredSlides={true}
-                   
-                    coverflowEffect={{
-                        rotate: 0,
-                        stretch: 0,
-                        depth: 50,
-                        modifier: 7,
-                    }}
-                    pagination={{el: '.swiper-pagination', clickable: true}}
-                    navigation={{
-                        nextEl: '.swiper-button-next',
-                        prevEl: '.swiper-button-prev',
-                    }}
-                    modules={[EffectCoverflow,Pagination,Navigation]}
-                    className="swiper_container"
-
-                    style={{width: "100rem"}}
-
-                    breakpoints={{
-                        640: {
-                            slidesPerView: 1,
-                        },
-                        768: {
-                            slidesPerView: 2,
-                        },
-                        1024: {
-                            slidesPerView: 3,
-                        },
-                    }}
-                >
-                    {photos.map((item, index) => (
-                        <SwiperSlide className=""><Image src={item.src} alt={item.alt} key={index}/></SwiperSlide>
-                    ))}
-                    {/* <SwiperSlide className="bg-center bg-cover block"><Image src={leftArrow} alt={"left arrow"} width={100}/></SwiperSlide>
+      <div className="flex items-center justify-center gap-7">
+        <Swiper
+          effect={"coverflow"}
+          centeredSlides={true}
+          coverflowEffect={{
+            rotate: 0,
+            stretch: 0,
+            depth: 50,
+            modifier: 7,
+          }}
+          pagination={{ el: ".swiper-pagination", clickable: true }}
+          navigation={{
+            nextEl: ".swiper-button-next",
+            prevEl: ".swiper-button-prev",
+          }}
+          modules={[EffectCoverflow, Pagination, Navigation]}
+          className="swiper_container"
+          style={{ width: "100rem" }}
+          breakpoints={{
+            640: {
+              slidesPerView: 1,
+            },
+            768: {
+              slidesPerView: 2,
+            },
+            1024: {
+              slidesPerView: 3,
+            },
+          }}
+        >
+          {photos.map((item, index) => (
+            <SwiperSlide className="">
+              <Image src={item.src} alt={item.alt} key={index} />
+            </SwiperSlide>
+          ))}
+          {/* <SwiperSlide className="bg-center bg-cover block"><Image src={leftArrow} alt={"left arrow"} width={100}/></SwiperSlide>
                     <SwiperSlide ><Image src={leftArrow} alt={"left arrow"} width={100}/></SwiperSlide>
                     <SwiperSlide ><Image src={leftArrow} alt={"left arrow"} width={100}/></SwiperSlide>
                     <SwiperSlide ><Image src={leftArrow} alt={"left arrow"} width={100}/></SwiperSlide>
@@ -84,14 +83,17 @@ const Snapshots = ({photos}: SnapshotsProps) => {
                     <SwiperSlide ><Image src={leftArrow} alt={"left arrow"} width={100}/></SwiperSlide>
  */}
 
-                    <div className="swiper-button-prev" ><Image src={leftArrow} alt={"left arrow"} width={100} ></Image></div>
-                    <div className="swiper-button-next"><Image src={rightArrow} alt={"right arrow"} width={100}></Image></div>  
-                    <div className="swiper-pagination"></div>
-                </Swiper>
-            </div> 
-        </div>
-        
-    );
+          <div className="swiper-button-prev">
+            <Image src={leftArrow} alt={"left arrow"} width={100}></Image>
+          </div>
+          <div className="swiper-button-next">
+            <Image src={rightArrow} alt={"right arrow"} width={100}></Image>
+          </div>
+          <div className="swiper-pagination"></div>
+        </Swiper>
+      </div>
+    </div>
+  );
 };
 
 export default Snapshots;
--- src/data/snapshots.ts
+++ src/data/snapshots.ts
@@ -2,41 +2,41 @@ import { StaticImageData } from "next/image";
 import photo1 from "@/public/images/who_we_are/snapshots2_img.webp";
 
 interface Photo {
-    src: StaticImageData;
-    alt: string;
+  src: StaticImageData;
+  alt: string;
 }
 
 export const snapshots: Photo[] = [
-    {
-        src: photo1,
-        alt: "photo1",
-    },
-    {
-        src: photo1,
-        alt: "photo1",
-    },
-    {
-        src: photo1,
-        alt: "photo1",
-    },
-    {
-        src: photo1,
-        alt: "photo1",
-    },
-    {
-        src: photo1,
-        alt: "photo1",
-    },
-    {
-        src: photo1,
-        alt: "photo1",
-    },
-    {
-        src: photo1,
-        alt: "photo1",
-    },
-    {
-        src: photo1,
-        alt: "photo1",
-    },
+  {
+    src: photo1,
+    alt: "photo1",
+  },
+  {
+    src: photo1,
+    alt: "photo1",
+  },
+  {
+    src: photo1,
+    alt: "photo1",
+  },
+  {
+    src: photo1,
+    alt: "photo1",
+  },
+  {
+    src: photo1,
+    alt: "photo1",
+  },
+  {
+    src: photo1,
+    alt: "photo1",
+  },
+  {
+    src: photo1,
+    alt: "photo1",
+  },
+  {
+    src: photo1,
+    alt: "photo1",
+  },
 ];
