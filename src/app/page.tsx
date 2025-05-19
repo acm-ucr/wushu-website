f9d22d59c75ab6d23fa5bcf6bad137b958cafe3e
Chris Jung <141481670+chrisjung0919@users.noreply.github.com>
2025-05-19 16:25:42 -0700

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,15 @@
-import WhoWeAre from "@/components/home/WhoWeAre";
-import LocationAndTime from "@/components/home/LocationAndTime";
-import JoinForm from "@/components/home/JoinForm";
+import Example from "@/components/Example";
+import MeetTheBoard from "@/components/board/BoardMemberCard";
 
 const Home = () => {
   return (
-    <div className="">
-      <WhoWeAre />
-      <LocationAndTime />
-      <JoinForm />
+    <div className="font-arimo flex h-screen w-screen items-center justify-center">
+      <Example text1="Adjust" text2="these" text3="Props" />
     </div>
   );
 };
 
 export default Home;
+export default function Home() {
+  return <MeetTheBoard />;
+}
