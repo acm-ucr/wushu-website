911603f139fabe1d4c3dfda0a217e412b371934a
CruzL092 <cruzbusiness760@gmail.com>
2025-05-19 18:11:56 -0700

    Used npm install --legacy-peer-deps

--- src/app/calendar/page.tsx
+++ src/app/calendar/page.tsx
@@ -2,7 +2,7 @@ import WushuCalendar from "../../components/events/calendar/calendar";
 
 const Calendar = () => {
   return (
-    <div className="flex h-screen w-screen items-center justify-center">
+    <div className="flex items-center justify-center">
       <WushuCalendar></WushuCalendar>
     </div>
   );
--- src/components/events/calendar/calendar.tsx
+++ src/components/events/calendar/calendar.tsx
@@ -7,8 +7,8 @@ import { Calendar } from "../../ui/calendar";
 
 const WushuCalendar = () => {
   return (
-    <div className="bg-wushu-cream-100 flex w-full flex-col items-center justify-center lg:pt-64 lg:pb-52">
-      <div className="mt-24 -mb-12 flex w-full justify-between px-12">
+    <div className="bg-wushu-cream-100 flex w-full flex-col items-center justify-center lg:pb-52">
+      <div className="mt-6 -mb-12 flex w-full justify-between px-12">
         <Image
           src={Border}
           alt="Border left"
