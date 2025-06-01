2e971175e009d5227b8a4887690bbe7b50ef3ba6
Simon <simoncun2586@gmail.com>
2025-05-31 22:09:10 -0700

    animation for the meet the board

--- src/components/board/MeetTheBoard.tsx
+++ src/components/board/MeetTheBoard.tsx
@@ -1,10 +1,31 @@
+"use client";
 import Image from "next/image";
+import { motion } from "framer-motion";
+
+const slideInVariant = {
+  initial: { opacity: 0, x: 20 },
+  animate: (custom: number) => ({
+    opacity: 1,
+    x: 0,
+    transition: {
+      duration: 0.8,
+      delay: custom,
+      ease: "backInOut",
+    },
+  }),
+};
 
 const MeetTheBoard = () => {
   return (
     <div className="flex flex-col items-center justify-center gap-10 px-6 py-12 md:flex-row">
-      {}
-      <div className="w-full md:w-1/2">
+      <motion.div
+        className="w-full md:w-1/2"
+        variants={slideInVariant}
+        initial="initial"
+        whileInView="animate"
+        viewport={{ once: true, amount: 0.5 }}
+        custom={0}
+      >
         <Image
           src="/images/meet_the_board/meet_the_board.webp"
           alt="Wushu Board Group Photo"
@@ -13,14 +34,39 @@ const MeetTheBoard = () => {
           priority
           className="border-wushu-gold-100 w-full rounded-lg border-4 shadow-lg"
         />
-      </div>
-      {}
+      </motion.div>
+
       <div className="w-full md:w-1/2">
-        <div className="text-wushu-gold-100 text-5xl leading-tight font-bold md:text-6xl lg:text-7xl">
-          <div>MEET THE</div>
-          <div>2024–2025</div>
-          <div>BOARD</div>
-        </div>
+        <motion.div
+          custom={0.15}
+          variants={slideInVariant}
+          initial="initial"
+          whileInView="animate"
+          viewport={{ once: true, amount: 0.5 }}
+          className="text-wushu-gold-100 text-5xl leading-tight font-bold md:text-6xl lg:text-7xl"
+        >
+          MEET THE
+        </motion.div>
+        <motion.div
+          custom={0.3}
+          variants={slideInVariant}
+          initial="initial"
+          whileInView="animate"
+          viewport={{ once: true, amount: 0.5 }}
+          className="text-wushu-gold-100 text-5xl leading-tight font-bold md:text-6xl lg:text-7xl"
+        >
+          2024–2025
+        </motion.div>
+        <motion.div
+          custom={0.45}
+          variants={slideInVariant}
+          initial="initial"
+          whileInView="animate"
+          viewport={{ once: true, amount: 0.5 }}
+          className="text-wushu-gold-100 text-5xl leading-tight font-bold md:text-6xl lg:text-7xl"
+        >
+          BOARD
+        </motion.div>
       </div>
     </div>
   );
