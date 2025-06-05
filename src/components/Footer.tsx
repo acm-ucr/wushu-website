cf89bed34f8d25293dd8cad1f88dce1b08b899dc
Simon <simoncun2586@gmail.com>
2025-06-05 10:33:53 -0700

    Hover Animation on Footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,10 +1,19 @@
+"use client";
 import cloudfooter from "@/public/logos/cloudfooter.svg";
 import logo from "@/public/logos/logo.svg";
 import Link from "next/link";
 import Image from "next/image";
 import gold from "@/public/logos/goldcorners.svg";
 import { links } from "@/data/footer";
+import { motion } from "framer-motion";
 
+const HoverAnimation = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <motion.div whileHover={{ scale: 1.2 }} whileTap={{ scale: 0.8 }}>
+      {children}
+    </motion.div>
+  );
+};
 const Footer = () => {
   return (
     <div className="relative w-full items-center">
@@ -36,9 +45,11 @@ const Footer = () => {
           </div>
 
           {links.map((item, index) => (
-            <Link href={item.link} target="_blank" key={index} className="">
-              <Image src={item.icon} alt="" />
-            </Link>
+            <HoverAnimation key={index}>
+              <Link href={item.link} target="_blank" key={index} className="">
+                <Image src={item.icon} alt="" />
+              </Link>
+            </HoverAnimation>
           ))}
           <div className="md:hidden">
             <Image
