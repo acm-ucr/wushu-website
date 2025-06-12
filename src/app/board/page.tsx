6f9f612dcd54f535f0a3cbb411a353a26ace6ed2
veritasia <aminaapstudent@yahoo.com>
2025-06-11 22:54:00 -0700

    adding gallery component separately

--- src/app/board/page.tsx
+++ src/app/board/page.tsx
@@ -3,12 +3,12 @@ import BoardMemberMap from "@/components/board/Cards";
 import LineBreak from "@/components/LineBreak";
 const Board = () => {
   return (
-    <div className="">
+    <>
       <MeetTheBoard />
       <LineBreak />
       <BoardMemberMap />
       <LineBreak />
-    </div>
+    </>
   );
 };
 
