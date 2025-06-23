74af81f7e298d2e14910a0e71beef1b4434e5c60
veritasia <aminaapstudent@yahoo.com>
2025-06-22 23:41:06 -0700

    adjusted board page

--- src/components/board/Card.tsx
+++ src/components/board/Card.tsx
@@ -1,6 +1,6 @@
 "use client";
 import Image, { StaticImageData } from "next/image";
-import { motion } from "framer-motion";
+import { motion } from "motion/react";
 
 interface BoardMemberCardProps {
   photo: StaticImageData;
