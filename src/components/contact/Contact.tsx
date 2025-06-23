9bdf5301a428b1e81f4426481c34fa1cb93263fc
Alton Qian <qian.alton@gmail.com>
2025-06-22 23:57:10 -0700

    aniamted contact us

--- src/components/contact/Contact.tsx
+++ src/components/contact/Contact.tsx
@@ -3,16 +3,16 @@ import Image from "next/image";
 import Link from "next/link";
 import Header from "@/components/Header";
 import wushuContactPic from "@/public/images/contact_us/contact_us.webp";
-import { motion } from "framer-motion"
+import { motion } from "framer-motion";
 
 const Contact = () => {
   return (
     <div className="bg-wushu-cream flex flex-col items-center justify-center gap-10 px-4 py-10 md:flex-row md:px-20">
       <div className="w-full md:w-1/2">
         <motion.div
-          initial={{opacity: 0, y: -20, x: -20}}
-          whileInView={{opacity: 1, y: 0, x: 0}}
-          transition={{duration: 0.7, delay: 0.2}}
+          initial={{ opacity: 0, y: -20, x: -20 }}
+          whileInView={{ opacity: 1, y: 0, x: 0 }}
+          transition={{ duration: 0.7, delay: 0.2 }}
           className=""
         >
           <Image
@@ -26,29 +26,31 @@ const Contact = () => {
 
       <div className="text-wushu-gold-100 w-full md:w-1/2">
         <motion.div
-          initial={{opacity: 0, y: -20, x: 20}}
-          whileInView={{opacity: 1, y: 0, x: 0}}
-          transition={{duration: 0.7, delay: 0.5}}
+          initial={{ opacity: 0, y: -20, x: 20 }}
+          whileInView={{ opacity: 1, y: 0, x: 0 }}
+          transition={{ duration: 0.7, delay: 0.5 }}
           className=""
         >
           <Header title="CONTACT US" isRed={false} />
         </motion.div>
         <div className="mt-5 space-y-3 text-base md:text-lg">
           <motion.div
-            initial={{opacity: 0, x: 20}}
-            whileInView={{opacity: 1, x: 0}}
-            transition={{duration: 0.7, delay: 0.7}}
+            initial={{ opacity: 0, x: 20 }}
+            whileInView={{ opacity: 1, x: 0 }}
+            transition={{ duration: 0.7, delay: 0.7 }}
             className=""
           >
             <p>
-              <span className="text-wushu-red-100 font-semibold">Instagram:</span>{" "}
+              <span className="text-wushu-red-100 font-semibold">
+                Instagram:
+              </span>{" "}
               @wushuatucr
             </p>
           </motion.div>
           <motion.div
-            initial={{opacity: 0, x: 20}}
-            whileInView={{opacity: 1, x: 0}}
-            transition={{duration: 0.7, delay: 0.9}}
+            initial={{ opacity: 0, x: 20 }}
+            whileInView={{ opacity: 1, x: 0 }}
+            transition={{ duration: 0.7, delay: 0.9 }}
             className=""
           >
             <p>
@@ -57,20 +59,22 @@ const Contact = () => {
             </p>
           </motion.div>
           <motion.div
-            initial={{opacity: 0, x: 20}}
-            whileInView={{opacity: 1, x: 0}}
-            transition={{duration: 0.7, delay: 1.1}}
+            initial={{ opacity: 0, x: 20 }}
+            whileInView={{ opacity: 1, x: 0 }}
+            transition={{ duration: 0.7, delay: 1.1 }}
             className=""
           >
             <p>
-              <span className="text-wushu-red-100 font-semibold">Facebook:</span>{" "}
+              <span className="text-wushu-red-100 font-semibold">
+                Facebook:
+              </span>{" "}
               @wushuatucr
             </p>
           </motion.div>
           <motion.div
-            initial={{opacity: 0, x: 20}}
-            whileInView={{opacity: 1, x: 0}}
-            transition={{duration: 0.7, delay: 1.3}}
+            initial={{ opacity: 0, x: 20 }}
+            whileInView={{ opacity: 1, x: 0 }}
+            transition={{ duration: 0.7, delay: 1.3 }}
             className=""
           >
             <p>
@@ -86,9 +90,9 @@ const Contact = () => {
             </p>
           </motion.div>
           <motion.div
-            initial={{opacity: 0, x: 20}}
-            whileInView={{opacity: 1, x: 0}}
-            transition={{duration: 0.7, delay: 1.5}}
+            initial={{ opacity: 0, x: 20 }}
+            whileInView={{ opacity: 1, x: 0 }}
+            transition={{ duration: 0.7, delay: 1.5 }}
             className=""
           >
             <p className="text-wushu-gold-100 text-sm italic">
